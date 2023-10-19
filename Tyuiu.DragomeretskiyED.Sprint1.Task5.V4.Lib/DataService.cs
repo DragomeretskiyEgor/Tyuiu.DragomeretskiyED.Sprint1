723dae7732200fef47b71bd6712ac53e884f281d
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-10-19 14:28:19 +0500

    Task5

---
+++ Tyuiu.DragomeretskiyED.Sprint1.Task5.V4.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.DragomeretskiyED.Sprint1.Task5.V4.Lib
+{
+    public class DataService : ISprint1Task5V4
+    {
+        public int SecondsToHours(int time)
+        {
+            double res = Math.Pow(time, 2) / Math.Sqrt(time);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DragomeretskiyED.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DragomeretskiyED.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpress()
+        {
+            int time = 3;
+            DataService ds = new DataService();
+            double res = ds.SecondsToHours(time);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 13257;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
