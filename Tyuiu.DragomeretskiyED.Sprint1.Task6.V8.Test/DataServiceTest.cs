209337db6965ef0f1f1b7357436be0fe25594719
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-10-19 14:32:33 +0500

    Task6

---
+++ Tyuiu.DragomeretskiyED.Sprint1.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DragomeretskiyED.Sprint1.Task6.V8.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint1.Task6.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string srt = "Пирожок";
+            DataService ds = new DataService();
+            string res = ds.MoveLetterToEnd(srt);
+            string wait = "Кирожоп";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DragomeretskiyED.Sprint1.Task6.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DragomeretskiyED.Sprint1.Task6.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DragomeretskiyED.Sprint1.Task6.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("552bdda5-af20-47e4-bfbf-9a75fa64013f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
